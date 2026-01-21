0c81cc3478265143ae207d9eb35b9a9e3d1783da
merinmathew2201 <merinmathewsuni@gmail.com>
2026-01-21 15:30:06 +0530

    ADD:home and recipes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { RouterLink } from "@angular/router";
+import { ApiServices } from '../services/api-services';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +12,19 @@ import { RouterLink } from "@angular/router";
 })
 export class Home {
 
+  allRecipes:any = signal([])
+  api = inject(ApiServices)
+
+  ngOnInit(){
+    this.getHomeRecipes()
+  }
+
+  getHomeRecipes(){
+    this.api.getAllRecipesAPI().subscribe((res:any)=>{
+      const homeRecipes = res.slice(0,6)
+      this.allRecipes.set(homeRecipes)
+      console.log(this.allRecipes());
+      
+    })
+  }
 }
--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import { ApiServices } from '../services/api-services';
 
 @Component({
   selector: 'app-recipes',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './recipes.html',
   styleUrl: './recipes.css',
 })
 export class Recipes {
+  mealTypeArray:any = signal([])
+  cuisineArray:any = signal([])
+  allRecipes:any = signal([])
+  dummyAllRecipe:any = []
+  api = inject(ApiServices)
 
+  ngOnInit(){
+    this.getAllRecipes()
+  }
+
+  getAllRecipes(){
+    this.api.getAllRecipesAPI().subscribe((res:any)=>{
+      this.allRecipes.set(res)
+      this.dummyAllRecipe = res
+      let dummyCuisineArray = res.map((item:any)=>item.cuisine)
+      dummyCuisineArray.forEach((cuisine:any)=>{
+        !this.cuisineArray().includes(cuisine) && this.cuisineArray().push(cuisine)
+      })
+      console.log(this.cuisineArray());
+      let dummyMealArray = res.map((item:any)=>item.mealType).flat(Infinity)
+      dummyMealArray.forEach((mealType:any)=>{
+        !this.mealTypeArray().includes(mealType) && this.mealTypeArray().push(mealType)
+      })
+      console.log(this.mealTypeArray());
+      
+    })
+  }
+
+  filterRecipe(key:string,value:string){
+    this.allRecipes.set(this.dummyAllRecipe.filter((item:any)=>item[key]==value))
+  }
 }
